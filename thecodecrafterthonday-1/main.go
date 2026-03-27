4acf552937ff068d0982499deb8f08171af05cd6
gimakolo <makoloeleojogift5@gmail.com>
2026-03-27 16:29:26 +0100

    i change my code to be able to handle error and also use the help command to show the user how to do it

--- thecodecrafterthonday-1/main.go
+++ thecodecrafterthonday-1/main.go
@@ -13,43 +13,108 @@ func main() {
 		fmt.Println("3. Multiply")
 		fmt.Println("4. Divide")
 		fmt.Println("5. exit")
+		fmt.Println("6.HELP")
 		fmt.Scan(&op)
 		if op == "5" {
-			fmt.Println("bye")
+			fmt.Println("Bye")
 			break
 		}
 
 		switch op {
 		case "1":
-			fmt.Println("Enter the first number: ")
-			fmt.Scan(&a)
+			for {
+				fmt.Println("Enter the first number: ")
+				_, error := fmt.Scan(&a)
+				if error != nil {
+					fmt.Println("You have an error: ")
+					fmt.Println(error)
+					continue
+				}
+				break
 
-			fmt.Println("Enter the second number: ")
-			fmt.Scan(&b)
+			}
+
+			for {
+				fmt.Println("Enter the second number: ")
+				_, error := fmt.Scan(&a)
+				if error != nil {
+					fmt.Println("You have an error: ")
+					fmt.Println(error)
+					continue
+				}
+				break
+
+			}
 
 			fmt.Println("Result:", a+b)
 		case "2":
-			fmt.Println("Enter the first number: ")
-			fmt.Scan(&a)
+			for {
+				fmt.Println("Enter the first number: ")
+				_, error := fmt.Scan(&a)
+				if error != nil {
+					fmt.Println("You have an error: ")
+					fmt.Println(error)
+					continue
+				}
+				break
+			}
 
-			fmt.Println("Enter the second number: ")
-			fmt.Scan(&b)
+			for {
+				fmt.Println("Enter the second number: ")
+				_, error := fmt.Scan(&a)
+				if error != nil {
+					fmt.Println("You have an error: ")
+					fmt.Println(error)
+					continue
+				}
+				break
+
+			}
 
 			fmt.Println("Result:", a-b)
 		case "3":
-			fmt.Println("Enter the first number: ")
-			fmt.Scan(&a)
-
-			fmt.Println("Enter the second number: ")
-			fmt.Scan(&b)
-
+			for {
+				fmt.Println("Enter the first number: ")
+				_, error := fmt.Scan(&a)
+				if error != nil {
+					fmt.Println("You have an error: ")
+					fmt.Println(error)
+					continue
+				}
+				break
+			}
+			for {
+				fmt.Println("Enter the second number: ")
+				_, error := fmt.Scan(&a)
+				if error != nil {
+					fmt.Println("You have an error: ")
+					fmt.Println(error)
+					continue
+				}
+				break
+			}
 			fmt.Println("Result:", a*b)
 		case "4":
-			fmt.Println("Enter the first number: ")
-			fmt.Scan(&a)
-
-			fmt.Println("Enter the second number: ")
-			fmt.Scan(&b)
+			for {
+				fmt.Println("Enter the first number: ")
+				_, error := fmt.Scan(&a)
+				if error != nil {
+					fmt.Println("You have an error: ")
+					fmt.Println(error)
+					continue
+				}
+				break
+			}
+			for {
+				fmt.Println("Enter the second number: ")
+				_, error := fmt.Scan(&a)
+				if error != nil {
+					fmt.Println("You have an error: ")
+					fmt.Println(error)
+					continue
+				}
+				break
+			}
 			if b != 0 {
 				fmt.Println("Result:", a/b)
 
@@ -57,6 +122,13 @@ func main() {
 			if b == 0 {
 				fmt.Println("cannot divide by 0 :")
 			}
+		case "6":
+			fmt.Println("How to use my calculator")
+			fmt.Println("1. add")
+			fmt.Println("2.sub")
+			fmt.Println("3.multiply")
+			fmt.Println("4.divide")
+			fmt.Println("5.exist")
 
 		default:
 			fmt.Println("Unknown operation")
