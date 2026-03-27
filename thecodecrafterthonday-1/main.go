3911fb0fc7d5090e6907debf625641b93e55faf4
gmakolo <makoloeleojogift5@gmail.com>
2026-03-27 12:10:40 +0100

    made changes to my calculator to stop loopigwhen the user enters a menue that isnt there

--- thecodecrafterthonday-1/main.go
+++ thecodecrafterthonday-1/main.go
@@ -12,25 +12,55 @@ func main() {
 		fmt.Println("2. Subtract")
 		fmt.Println("3. Multiply")
 		fmt.Println("4. Divide")
+		fmt.Println("5. exit")
 		fmt.Scan(&op)
-
-		fmt.Println("Enter the first number: ")
-		fmt.Scan(&a)
-
-		fmt.Println("Enter the second number: ")
-		fmt.Scan(&b)
+		if op == "5" {
+			fmt.Println("bye")
+			break
+		}
 
 		switch op {
 		case "1":
+			fmt.Println("Enter the first number: ")
+			fmt.Scan(&a)
+
+			fmt.Println("Enter the second number: ")
+			fmt.Scan(&b)
+
 			fmt.Println("Result:", a+b)
 		case "2":
+			fmt.Println("Enter the first number: ")
+			fmt.Scan(&a)
+
+			fmt.Println("Enter the second number: ")
+			fmt.Scan(&b)
+
 			fmt.Println("Result:", a-b)
 		case "3":
+			fmt.Println("Enter the first number: ")
+			fmt.Scan(&a)
+
+			fmt.Println("Enter the second number: ")
+			fmt.Scan(&b)
+
 			fmt.Println("Result:", a*b)
 		case "4":
-			fmt.Println("Result:", a/b)
+			fmt.Println("Enter the first number: ")
+			fmt.Scan(&a)
+
+			fmt.Println("Enter the second number: ")
+			fmt.Scan(&b)
+			if b != 0 {
+				fmt.Println("Result:", a/b)
+
+			}
+			if b == 0 {
+				fmt.Println("cannot divide by 0 :")
+			}
+
 		default:
 			fmt.Println("Unknown operation")
+
 		}
 	}
 }
